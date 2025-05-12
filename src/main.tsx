9b5d2682e35eec9af144baa1b7227cf56abfc3a1
andreyb <andrey.batasov@yahoo.com>
2025-05-12 21:40:58 +0000

    Added devcontainer

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import posthog from 'posthog-js';
+
+posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
+    api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
